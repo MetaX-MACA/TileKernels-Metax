95b0eb2e80842f7af98a957618ecdce4267c2d8b
suwei.ye <suwei.ye@metax-tech.com>
2026-07-07 19:06:28 +0800

    [MetaXGPU][mhc] Optimize kernels in Mhc.

--- tile_kernels/mhc/post_kernel.py
+++ tile_kernels/mhc/post_kernel.py
@@ -29,10 +29,6 @@ def _mhc_post_fwd(mhc: int, hidden: int, n_thr: int = 128, h_blk: int = 1024) ->
         x: T.Tensor[(n, mhc, h), T.bfloat16],
     ) -> None:
         with T.Kernel(n, threads=n_thr) as pid_n:
-            x_shared = T.alloc_shared((mhc, h_blk), T.bfloat16)
-            b_shared = T.alloc_shared((mhc, h_blk), T.bfloat16)
-            d_shared = T.alloc_shared(h_blk, T.bfloat16)
-
             x_local = T.alloc_fragment((mhc, h_blk), T.float32)
             b_local = T.alloc_fragment((mhc, h_blk), T.float32)
             d_local = T.alloc_fragment(h_blk, T.float32)
@@ -43,18 +39,14 @@ def _mhc_post_fwd(mhc: int, hidden: int, n_thr: int = 128, h_blk: int = 1024) ->
             T.copy(c[pid_n, 0], c_local)
 
             for i0_h in T.Pipelined(T.ceildiv(h, h_blk), num_stages=2):
-                T.copy(b[pid_n, 0, i0_h * h_blk], b_shared, disable_tma=True)
-                T.copy(d[pid_n, i0_h * h_blk], d_shared, disable_tma=True)
+                T.copy(b[pid_n, 0, i0_h * h_blk], b_local, disable_tma=True)
+                T.copy(d[pid_n, i0_h * h_blk], d_local, disable_tma=True)
 
-                T.copy(b_shared, b_local)
-                T.copy(d_shared, d_local)
                 for i_mhco, i1_h in T.Parallel(mhc, h_blk):
                     x_local[i_mhco, i1_h] = c_local[i_mhco] * d_local[i1_h]
                     for i_mhci in T.serial(mhc):
                         x_local[i_mhco, i1_h] += a_local[i_mhci, i_mhco] * b_local[i_mhci, i1_h]
-                T.copy(x_local, x_shared)
-
-                T.copy(x_shared, x[pid_n, 0, i0_h * h_blk], disable_tma=True)
+                T.copy(x_local, x[pid_n, 0, i0_h * h_blk], disable_tma=True)
 
     return _mhc_post_fwd_kernel
 
